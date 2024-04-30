4e430a00bed0480e63ae43f3f8fe82af21d360e8
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-04-30 16:38:44 +0000

    ...

---
+++ Q_1.py
@@ -0,0 +1,17 @@
+# Program to add two numbers.
+
+'''program with pre-defined variables'''
+num1 = 22;
+num2 = 55;
+
+sum = num1 + num2;
+
+print("The sum of two numbers is : ",sum);
+
+'''program with user-defined'''
+num1 = float(input("Enter the first number : "));
+num2 = float(input("Enter the second number : "));
+
+sum = num1 + num2;
+
+print("The sum of two numbers is : ",sum);
