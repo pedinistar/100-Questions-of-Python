37deecf2a895efcd6e8b24600dce6fa3e648937a
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2024-04-30 22:31:18 +0530

    Questions Commited

---
+++ Question-3.py
@@ -0,0 +1,13 @@
+# Swap Numbers
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+print("\n")
+print(f"The First Number is {a}")
+print(f"The Second Number is {b}")
+
+a, b = b, a
+print("Swapping./././")
+
+print(f"The First Number is {a}")
+print(f"The Second Number is {b}")
+
---
+++ Square_root.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the number: "))
+
+square_root = number**0.5
+
+print(f"The square root of {number} is {square_root}")
+             
