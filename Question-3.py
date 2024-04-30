0fdb6126d0d00ec3a50fb38d4e125e7c9f250357
YuiSurbhi <163987279+YuiSurbhi@users.noreply.github.com>
2024-04-30 23:01:22 +0530

    Update Question-3.py

--- Question-3.py
+++ Question-3.py
@@ -11,3 +11,15 @@ print("Swapping./././")
 print(f"The First Number is {a}")
 print(f"The Second Number is {b}")
 
+'''swap two numbers using third variable'''
+x = 12;
+b = 14;
+
+temp = x;
+print("The value of temp variable is : ",temp);
+
+x = y;
+print("The value of x variable is : ",x);
+
+y = temp;
+print("The value of y variable is : ",y);
